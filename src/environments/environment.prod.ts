310f8eb7cc60e8bb315f6b0e286c3e755fca9c8c
Param <parampreet@digisoftsolution.com>
2025-10-03 16:48:59 +0530

    captcha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://3.128.142.109/8080',
   subscriptionUrl:'http://3.128.142.109:4200',
-  captchaSiteKey:'6Lc--NorAAAAAF4rMGkhcvA--o03bUkdJQFRbTbt'
+  captchaSiteKey:'6Le-KN0rAAAAALdkJi-8VN1MzmMQfVGDNpiF9xT6'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: "http://3.128.142.109/8080",
   //apiUrl: 'https://localhost:63334',
   subscriptionUrl:'http://3.128.142.109:4200',
-  captchaSiteKey:'6Lc--NorAAAAAF4rMGkhcvA--o03bUkdJQFRbTbt'
+  captchaSiteKey:'6Le-KN0rAAAAALdkJi-8VN1MzmMQfVGDNpiF9xT6'
 };
