711fc97a2bec218befdc81475f51aca0407590cb
Param <parampreet@digisoftsolution.com>
2025-10-06 14:52:10 +0530

    changes

---
+++ src/app/core/models/CityVM.ts
@@ -0,0 +1,19 @@
+export interface CityVM extends AddUpdateCityDto {
+  isActive: boolean;
+  createdDate: string;   // ISO date string from backend
+  updatedDate?: string | null;
+  isDeleted: boolean;
+  assignedBy?: string;
+  userCityMappingID?:number;
+  score?: number;
+}
+export interface AddUpdateCityDto {
+  cityID: number;
+  country: string;
+  state: string;
+  cityName: string;
+  postalCode: string;
+  region: string;
+  image: string;
+}
+
--- src/app/core/services/public-service.ts
+++ src/app/core/services/public-service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpService } from './http-service';
 import { ResultResponseDto } from '../models/ResultResponseDto';
 import { map } from 'rxjs';
+import { CityVM } from '../models/CityVM';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,8 @@ export class PublicService {
   contactus(data: any) {
     return this.http.post('Auth/contactus', data).pipe(map((x) => x as ResultResponseDto<unknown>));
   }
-
-
+  
+  getAllCityByUserId() {
+    return this.http.get('City/getAllCityByUserId').pipe(map((x) => x as ResultResponseDto<CityVM[]>));
+  }
 }
