a789a4937dabeff48305d70354c19f33386884bf
Param <parampreet@digisoftsolution.com>
2025-10-06 17:45:04 +0530

    url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://3.128.142.109/8080',
-  subscriptionUrl:'http://3.128.142.109:4200',
+  subscriptionUrl:'http://veridianurbansystems.com',
   captchaSiteKey:'6Le-KN0rAAAAALdkJi-8VN1MzmMQfVGDNpiF9xT6'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: "http://3.128.142.109/8080",
-  //apiUrl: 'https://localhost:63334',
-  subscriptionUrl:'http://3.128.142.109:4200',
+ // apiUrl: 'https://localhost:63334',
+  subscriptionUrl:'http://veridianurbansystems.com',
   captchaSiteKey:'6Le-KN0rAAAAALdkJi-8VN1MzmMQfVGDNpiF9xT6'
 };
