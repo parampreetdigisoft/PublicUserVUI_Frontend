4a419951fede2d3f95df7e33034c01fec0603b4d
Param <parampreet@digisoftsolution.com>
2025-10-07 14:52:55 +0530

    filter changes

--- src/app/features/partner-city/partner-city.ts
+++ src/app/features/partner-city/partner-city.ts
@@ -1,30 +1,57 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonService } from '../../core/services/common-service';
 import { UserRoleValue } from '../../core/models/UserRole';
 import { pillarsData } from '../../core/data/pillarData';
 import { CommonModule } from '@angular/common';
+import { countriesList } from '../../core/data/countriesList';
+import { USACity } from '../../core/data/USACity';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-partner-city',
-  imports: [CommonModule],
+  imports: [CommonModule, NgSelectModule, ReactiveFormsModule],
   templateUrl: './partner-city.html',
   styleUrl: './partner-city.css'
 })
-export class PartnerCity {
-private common = inject(CommonService);
-pillars = pillarsData;
+export class PartnerCity implements OnInit {
+  private common = inject(CommonService);
+  pillars = pillarsData;
+  cities: string[] = USACity;
+  countries = countriesList;
+  filterForm!: FormGroup;
+  regions: string[] = ['North America', 'Europe', 'Asia', 'Australia', 'Africa', 'South America', 'Antarctica'];
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.filterForm = this.fb.group({
+      country: [null],
+      region: [],
+      city: [null],
+      pillar: []
+    });
+  }
 
   loginCityUser() {
     let url = '/auth/login';
     this.common.goToSubscriptionApp(url);
   }
-  
+
   goToSite() {
     this.common.goToSubscriptionApp();
   }
-  
+
   loginAdmin() {
     let url = '/auth/login?role=' + UserRoleValue.Admin;
     this.common.goToSubscriptionApp(url);
+  }
+  onCountryChange(selectedCountry: any) {
+    if (selectedCountry) {
+      this.cities = selectedCountry.cities;
+
+    }
+  }
+  onSearch() {
+    console.log('Form Value:', this.filterForm.value);
+    // Implement your search/filter logic here
   }
 }
