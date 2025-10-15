9327aa17507bd395d4277e36fa80aa8080544ed2
Param <parampreet@digisoftsolution.com>
2025-10-15 19:32:20 +0530

    https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "http://3.128.142.109:8080",
+  apiUrl: "https://3.128.142.109:8080",
   //apiUrl: 'https://localhost:63334',
   subscriptionUrl:'http://veridianurbansystems.com',
   captchaSiteKey:'6Le-KN0rAAAAALdkJi-8VN1MzmMQfVGDNpiF9xT6'
