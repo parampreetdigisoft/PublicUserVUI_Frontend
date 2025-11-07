6c0498dee45020769293181e6f55d872513a4afd
Param <parampreet@digisoftsolution.com>
2025-11-07 10:03:21 +0530

    changes

--- src/app/features/blog-component/container/blog-detail-component/blog-detail-component.ts
+++ src/app/features/blog-component/container/blog-detail-component/blog-detail-component.ts
@@ -1,10 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CommonService } from '../../../../core/services/common-service';
 import { UserRoleValue } from '../../../../core/models/UserRole';
+import { Blog1 } from '../blog1/blog1';
+import { Blog2 } from '../blog2/blog2';
+import { Blog3 } from '../blog3/blog3';
+import { Blog4 } from '../blog4/blog4';
+import { Blog5 } from '../blog5/blog5';
 
 @Component({
   selector: 'app-blog-detail-component',
-  imports: [],
+  imports: [Blog1, Blog2, Blog3, Blog4, Blog5],
   templateUrl: './blog-detail-component.html',
   styleUrl: './blog-detail-component.css'
 })
