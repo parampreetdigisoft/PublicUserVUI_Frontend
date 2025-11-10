5e9a3349825445ca425ccaa5aa3222201d9c1764
Param <parampreet@digisoftsolution.com>
2025-11-10 13:34:54 +0530

    toast msg error fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { FooterComponent } from './shared/components/footer-component/footer-com
 import { ToasterService } from './core/services/toaster.service';
 import { Subject, takeUntil } from 'rxjs';
 import { CommonModule } from '@angular/common';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,RouterOutlet, HearderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HearderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -18,32 +19,44 @@ export class App {
   protected readonly title = signal('PublicVUI_Frontend');
   toastMessage = '';
   toastClass = '';
-  showToast = signal(false);
-  private destroy$ = new Subject();
+  private destroy$ = new Subject<void>();
   constructor(private toasterService: ToasterService) { }
+  ngOnInit(): void {
+    this.toasterService.success$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(message => this.showToaster(message, 'success'));
+
+    this.toasterService.error$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(message => this.showToaster(message, 'danger'));
+
+    this.toasterService.info$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(message => this.showToaster(message, 'info'));
+
+    this.toasterService.warning$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(message => this.showToaster(message, 'warning'));
+  }
+
   ngOnDestroy(): void {
+    this.destroy$.next();
     this.destroy$.complete();
   }
-  ngOnInit(): void {
-    this.toasterService.success$.pipe(takeUntil(this.destroy$))
-      .subscribe(message => this.showToaster(message, 'success'));
-    this.toasterService.error$.pipe(takeUntil(this.destroy$))
-      .subscribe(message => this.showToaster(message, 'danger'));
-    this.toasterService.info$.pipe(takeUntil(this.destroy$))
-      .subscribe(message => this.showToaster(message, 'info'));
-    this.toasterService.warning$.pipe(takeUntil(this.destroy$))
-      .subscribe(message => this.showToaster(message, 'warning'));
 
-  }
-
-
-  private showToaster(message: string, className: string) {
-    this.showToast.set(true);
+  private showToaster(message: string, type: string) {
     this.toastMessage = message;
-    this.toastClass = className;
-    setTimeout(() => {
-      this.showToast.set(false);
-    }, 3000);
+    this.toastClass = type;
+
+    const toastEl = document.getElementById('liveToastBtn');
+    if (!toastEl) return;
+
+    // Remove all old type classes first
+    toastEl.classList.remove('success', 'danger', 'info', 'warning');
+    toastEl.classList.add(type);
+
+    const toast = bootstrap.Toast.getOrCreateInstance(toastEl, { delay: 2000 });
+    toast.show();
   }
 
   isRouteLoading = true;
